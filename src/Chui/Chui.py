14fae9333120082878163f824a2035a342f03907
Daniel Wong <dwong56@calpoly.edu>
2017-06-24 15:56:02 -0700

    Script working properly for detecting faces and spoofs

--- src/Chui/Chui.py
+++ src/Chui/Chui.py
@@ -8,7 +8,7 @@ def spoofDetection(pictureStream):
        "Content-Type":"image/jpeg",
     }
     url = "https://api.chui.ai/v1/spdetect"
-    r  = requests.post(url,data=pictureStream,headers=headers)
+    r  = requests.post(url, data = pictureStream, headers = headers)
     print(r.json())
 
 # Enroll a new user. Params(3 pictures) returns enrollment
@@ -19,17 +19,26 @@ def enrollUser(img0, img1, img2, name):
     }
     url = "https://api.chui.ai/v1/enroll"
     data = {
-    "img0":base64.b64encode(img0),
-    "img1":base64.b64encode(img1),
-    "img2":base64.b64encode(img2),
-    "name":name
+    "img0":base64.b64encode(open('Jinchi1.jpg', 'rb').read()),
+    "img1":base64.b64encode(open('Jinchi2.jpg', 'rb').read()),
+    "img2":base64.b64encode(open('Jinchi3.jpg', 'rb').read()),
+    "name":"Jinchi"
     }
-
-    r = requests.post(url, data=json.dumps(data), headers = headers)
-
+    r = requests.post(url, data=json.dumps(data), headers=headers)
     print(r.json())
 
-
+def faceMatch(enrollmentID, pictureStream):
+    headers = {
+       "x-api-key":"vOjf0XRyf72QJzFOVxff7aKYtUeRBtgR6MXAMzPe",
+       "Content-Type":"application/json",
+    }
+    url = "https://api.chui.ai/v1/match"
+    data = {
+        "img":base64.b64encode(pictureStream),
+        "id":enrollmentID
+    }
+    r = requests.post(url, data=json.dumps(data), headers=headers)
+    print(r.json())
 
 
 
