2eb7312205a126a536fdbb09f971322cad7a97ca
Daniel Wong <dwong56@calpoly.edu>
2017-06-24 14:58:15 -0700

    Tester script is now online. Please refer to it when sending in pictures for Chui srvices

--- src/Chui/Chui.py
+++ src/Chui/Chui.py
@@ -1,23 +1,36 @@
 import requests
+import base64
+import json
 
-def spoofDetection():
+def spoofDetection(pictureStream):
     headers = {
        "x-api-key":"vOjf0XRyf72QJzFOVxff7aKYtUeRBtgR6MXAMzPe",
        "Content-Type":"image/jpeg",
     }
     url = "https://api.chui.ai/v1/spdetect"
-    r  = requests.post(url,data=open('/Users/brehski/GlobalAI/pics/Jinchi1.jpg','rb').read(),headers=headers)
-    print r.json()
+    r  = requests.post(url,data=pictureStream,headers=headers)
+    print(r.json())
 
-"""def enrollUser()
+# Enroll a new user. Params(3 pictures) returns enrollment
+def enrollUser(img0, img1, img2, name):
     headers = {
        "x-api-key":"vOjf0XRyf72QJzFOVxff7aKYtUeRBtgR6MXAMzPe",
        "Content-Type":"application/json",
     }
-
     url = "https://api.chui.ai/v1/enroll"
-"""
+    data = {
+    "img0":base64.b64encode(img0),
+    "img1":base64.b64encode(img1),
+    "img2":base64.b64encode(img2),
+    "name":name
+    }
+
+    r = requests.post(url, data=json.dumps(data), headers = headers)
+
+    print(r.json())
 
 
-spoofDetection()
+
+
+
 
